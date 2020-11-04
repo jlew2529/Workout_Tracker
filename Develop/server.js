2eff834a31a8085004d765d97cbcce3063ba2d9c
Jalen Lewis <jalenlew1995@gmail.com>
2020-11-04 15:56:23 -0600

    deploying

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+// Set up the express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+// Set up the express app data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Set up MongoDB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/Workout_Tracker",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+);
